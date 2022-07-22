6026c981b654068a1a3dc891a4873d50541e8359
“sydasif” <sydasif78@gmail.com>
2022-07-22 13:21:09 +0500

    Add print exe

---
+++ ex01.py
@@ -0,0 +1,8 @@
+# Ex01 date: 21 Jul, 2022
+print("Hello World")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "Said" do not touch this.')
