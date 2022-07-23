9859a4ca3c0bb681a791cef73f3049fb1f6bf9db
“sydasif” <sydasif78@gmail.com>
2022-07-23 17:53:11 +0500

    spell mistake

--- ex07_print.py
+++ ex07_print.py
@@ -1,6 +1,6 @@
 print("Mary had a little lamb.")
 print("Its fleece was white as {}.".format('snow'))
-print("And everywhere that Mary went.")
+print("And every where that Mary went.")
 print("." * 10) # what'd that do?
 
 end1 = "C"
