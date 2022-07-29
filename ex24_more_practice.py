c56788498df937d062fe59d6f1fc8c1c00a7d932
“sydasif” <sydasif78@gmail.com>
2022-07-29 18:14:57 +0500

    practices more python function

---
+++ ex24_more_practice.py
@@ -0,0 +1,42 @@
+print("Let's parctice everything.")
+print('You\'d need to know \'about escapes with \\ that do:')
+print('\nnewlines and \ttabs')
+
+poem = """
+\tThe lovly world
+with logic so firmly planted
+cannot discern \nthe needs of love
+nor comprehend passion from intution
+and requires an explanation
+\n\twhere ther is none.
+"""
+
+print("-----------------")
+print(poem)
+print("------------------")
+
+
+five = 10 - 2 + 3 -6
+print(f"This should be five {five}")
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+# remember that this is another way to format a string
+print("With a starting point of: {}".format(start_point))
+# its just like with an f"" string
+print(f"We'd have Beans: {beans}, Jars: {jars} and Crates: {crates}")
+
+strat_point = start_point / 10
+
+formula = secret_formula(start_point)
+print("We can also do that this way")
+# this is an easy way to apply a list to a fromat string
+print("Beans: {}, Jars: {}, Crates: {}".format(*formula))
