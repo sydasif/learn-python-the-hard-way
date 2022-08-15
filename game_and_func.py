e783a62f643a6029339a014381546a294f7c3764
syed asif <sydasif78@gmail.com>
2022-08-15 20:10:36 +0500

    update game function

--- game_and_func.py
+++ game_and_func.py
@@ -1,20 +1,55 @@
 from sys import exit
 
 def gold_room():
-    pass
+    print("You are in gold room.")
+    print("Gold room is full of gold.")
+    print("How much do you take?")
+    choice = int(input("> "))
+    if choice == 1:
+        print("Good, take it.")
+    elif choice == 2:
+        print("You win, take it.")
+    else:
+        if choice < 50:
+            print("Take it.")
+        else:
+            dead("You Greedy Man.")    
 
 
 def silver_room():
-    pass
+    print("chatalu is dead, and you are in silver room.")
+    print("This room is full of silver.")
+    print("Take it as you want. Bye!")
+    exit(0)
 
 
 def bear_room():
-    pass
+    print("You are in a bear room.")
+    print("There is a huge bear in room.")
+    print("Give it 'honey' or 'slap' the bear.")
+    choice = input("> ")
+    if choice == "honey":
+        print("You saved your life.")
+        gold_room()
+    else:
+        dead("You are dead man.")
+
 
 
 
 def chatalu_room():
-    pass
+    print("You are in chatalu room.")
+    print("A huge monster.")
+    print("What to do now. Go 'back' or 'fight'.")
+    
+    while True:    
+        choice = input("> ")
+        if choice == 'back':
+            dead("You suck!")
+        elif choice == 'fight':
+            silver_room()
+        else:
+            print("No way fight or go back.")
 
 
 def dead(why):
